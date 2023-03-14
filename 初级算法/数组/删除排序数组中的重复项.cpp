6e5ea03f9e91fff1e7df0122a1d7b8167515e256
SuperNinja801 <67430027+SuperNinja801@users.noreply.github.com>
2023-03-14 22:15:59 +0800

    Update and rename 删除排序数组中的重复项 to 删除排序数组中的重复项.cpp

---
+++ 初级算法/数组/删除排序数组中的重复项.cpp
@@ -0,0 +1,19 @@
+/*删除排序数组中的重复项  方法：快慢指针*/
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) {
+            return 0;
+        }
+        int fast = 1, slow = 1;
+        while (fast < n) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+};
